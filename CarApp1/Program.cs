1ca8fd7296518f92325bef8140f6b97c97dd0f50
Nicholas Smith <42845984+nicholasJsmith1995@users.noreply.github.com>
2018-11-02 07:34:40 -0700

    Add files via upload

---
+++ CarApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using car_class;
+
+namespace CarApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car(5);
+            Car car2 = new Car(6);
+
+            for (int i = 0; i < 5; i++)
+            {
+                car1.FerraiAccelerate();
+                Console.WriteLine("Ferrari "+car1.Speed);
+                
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                car1.Brake();
+                Console.WriteLine("Ferrai "+car1.Speed);
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                car2.LamboAccelerate();
+                Console.WriteLine("Lambo "+car2.Speed);
+            }
+            
+            for (int i = 0; i < 5; i++)
+            {
+                car2.Brake();
+                Console.WriteLine("Lambo "+car2.Speed);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ car class/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_class
+{
+    
+    public class Car
+    {
+        
+       
+
+        public int YearModel { get; set; }
+        public string Make { get; set; }
+        public int Speed { get; set; }
+        public Car() { }
+
+        public Car(int yearModel, string Make)
+        {
+            this.YearModel = YearModel = yearModel;
+            this.Make = Make;
+            this.Speed = 0;
+        }
+        public Car(int speed)
+        {
+            this.Speed = speed;
+        }
+        public void FerraiAccelerate()
+        {
+            Random getrandom = new Random();
+            this.Speed += getrandom.Next(0, 20);
+        }
+        public void LamboAccelerate()
+        {
+            Random getrandom2 = new Random();
+            this.Speed += getrandom2.Next(0, 19);
+        }
+        public void Brake()
+        {
+            this.Speed -= 5;
+        }
+    }
+}   
